362b2c1637b2ad80b64991a8778b26eb60085247
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-18 12:01:54 +0530

    programs

---
+++ HomeController.java
@@ -0,0 +1,30 @@
+package com.codegnan.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+	@RequestMapping(value="/java")
+	public String viewJava() {
+		return "java";
+	}
+	@RequestMapping(value="/python")
+	public String viewPython() {
+		return "python";
+	}
+	@RequestMapping(value="/spring")
+	public String viewSpring() {
+		return "spring";
+	}
+}
