c4776a5597abf2cecfa56af426c9d1cb3f98c0d6
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-25 22:41:28 +0530

    programs

---
+++ EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.codegnan.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.codegnan.model.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
