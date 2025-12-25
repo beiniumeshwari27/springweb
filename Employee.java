9543b9292c15755ae0e912624e5d0deb1b7d5742
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-25 22:39:03 +0530

    programs

---
+++ Employee.java
@@ -0,0 +1,31 @@
+package com.codegnan.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String department;
+	private double salary;
+
+
+}
